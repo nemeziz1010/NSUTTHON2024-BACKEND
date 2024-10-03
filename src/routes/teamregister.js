91e5e0c4df61a65ea0b402bd8c1cdd7f28ed031c
nemeziz1010 <mitukorochikar10@gmail.com>
2024-10-04 00:40:22 +0530

    MAIL UPDATE

--- src/routes/teamregister.js
+++ src/routes/teamregister.js
@@ -149,27 +149,34 @@ router.post("/register", async (req, res) => {
         from: `"Team Crosslinks" <${SMTP_EMAIL}>`,
         to: teamLeader.email,
         subject: "Registration Successful",
-        html: `
-    <div style="background-color: #f4f4f4; padding: 20px; font-family: Arial, sans-serif;">
+        html: `<div style="background-color: #f4f4f4; padding: 20px; font-family: Arial, sans-serif;">
       <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 8px; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
         <h1 style="color: #333; font-size: 24px; border-bottom: 2px solid #eee; padding-bottom: 10px; margin-bottom: 20px;">Dear Team Leader,</h1>
         <p style="color: #666; font-size: 16px; line-height: 1.5;">Congratulations on successfully registering your team for NSUTTHON! Your team ID is ${teamId}.</p>
         <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Make sure you note down your team ID, as it will be used while registering for the events and to identify your team throughout.</p>
         <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">In order to get your team verified, and complete the registration process, carefully go through the instructions given below:</p>
+        <p className="mt-4" style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">
+          Carefully go through the instructions given below:
+          </p>
         <ol style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 10px;">
-          <li>Every team member needs to register on the app link given below, and complete the free aptitude test. Marks of the test do not matter. <a href="https://proedge.app.link/picxele2" style="color: #007BFF; text-decoration: none;">App Link (supported on android only)</a></li>
-          <li>It is the sole responsibility of the team leader to get every member of their team registered.</li>
-          <li>Team leaders need to submit a screenshot of each member's test score on the <a href="https://forms.gle/jZ1ApAk9VRQqwmc99" style="color: #007BFF; text-decoration: none;">form link attached</a>.</li>
-          <li>Please note that it is compulsory for each member to register using a single e-mail ID from one device, multiple entries from the same device are prohibited.</li>
-          <li>The app is supported by android devices only, use any android device available at your ease.</li>
-          <li>Make sure you use your personal email ID and not the college ID.</li>
+          <li>Every team member must register on the stockgro website <a className="font-bold text-blue-500"href="https://community.stockgro.club/form/10312eef-5387-439b-a6fa-d382a1fef702">(click here)</a> and create a profile using their personal email id to complete the first leg of your registration.</li>
+          <li>It is the team leader's responsibility to ensure all members complete the task and then submit their screenshots in the google form to completely register and be a part of NSUTTHON.</li>
+          <li>
+            For the second part of the registration process, you must fill out this short survey  <a className="font-bold text-blue-500"href="https://docs.google.com/forms/d/e/1FAIpQLSftK9XFBIP7KF4kQriCuNqciEta4iw4sjr3CA5mmiAaclFrRA/viewform">(click here)</a>, and enter details of an school-going child you know, it can be your sibling, relative or neighbour.
+            </li>
         </ol>
-        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Failing to complete any of the given steps will lead to the disqualification of the team.</p>
-        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">In case of any queries, feel free to reach out to us.</p>
-        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Regards,</p>
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">As the team captain, you're responsible for ensuring all team members have registered and you have to submit a screenshot of the same on the google form, positively.</p>
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Note: Each member can only register using one email per device. Multiple entries from the same device are prohibited.</p>
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">If your team is unable to complete the tasks, your team will unfortunately be disqualified.</p>
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">If you have any questions or concerns, please feel free to reach out to us. A link to our Whatsapp group is attached here with:
+            <a href="https://chat.whatsapp.com/IOKnp0w5GhV7wopGc8StZs" target="_blank" rel="noopener noreferrer" className="font-bold"> Whatsapp Group</a>.</p>
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Complete your registration by filling out this Google form: 
+          <a href="https://forms.gle/BSJfaaLhv1jt378Y9" target="_blank" rel="noopener noreferrer"> https://forms.gle/BSJfaaLhv1jt378Y9</a>.</p>
+
+        <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Best Regards,</p>
         <p style="color: #666; font-size: 16px; line-height: 1.5; margin-top: 20px;">Team Crosslinks</p>
       </div>
-    </div>
+    </div
     `,
         };
 
